8a4dcca093a4b00b388f6ee3dc5949d741ce364e
Jun <min2jn99@gmail.com>
2022-11-06 10:02:41 +0700

    Set up base styles and react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// Import library
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// Import styles
+import "./sass/styles.scss";
+
+// Import component
+
+function App() {
+    return (
+        <Router>
+            <h1>Hello</h1>
+        </Router>
+    );
+}
+
+export default App;
