9cea7cef70c371f1b7baaca4d69d7669a3d49300
Jun <min2jn99@gmail.com>
2022-11-24 11:53:18 +0700

    Update order success page

--- src/pages/client/Order/OrderSuccess.jsx
+++ src/pages/client/Order/OrderSuccess.jsx
@@ -1,12 +1,37 @@
 // Import library
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Import components
+import SuccessfulPurchase from "../../../assets/successful_purchase.svg";
 
 const OrderSuccess = () => {
     return (
-        <div>
-            <div>OrderSuccess</div>
+        <div className="w-full h-full d-flex justify-center items-center">
+            <div className="text-center flow">
+                <img
+                    style={{
+                        width: "min(100%, 15rem)",
+                        aspectRatio: 1,
+                        objectFit: "cover",
+                        marginInline: "auto",
+                    }}
+                    src={SuccessfulPurchase}
+                    alt="Your order is confirmed!"
+                />
+                <p className="fs-400 fw-semibold">
+                    Đơn hàng của bạn đã được xác nhận
+                </p>
+                <p>Cảm ơn vì bạn đã tin tưởng và đặt hàng tại cửa hàng</p>
+                <Link
+                    to="/shop"
+                    className="button"
+                    button-variant="contained"
+                    button-color="primary"
+                >
+                    Tiếp tục mua hàng
+                </Link>
+            </div>
         </div>
     );
 };
